f03145935f51b96005eccc80277e1792078e0495
Rain6435 <brosimo@outlook.fr>
2023-03-06 18:51:17 -0500

    add db script

---
+++ wineshop.sql
@@ -0,0 +1,44 @@
+create database wineshop;
+
+
+create table wineshop.client (
+    ID int not null auto_increment, 
+    FirstName varchar(50),
+    LastName varchar(50), 
+    Age int, 
+    EMail text, 
+    AdressNum text, 
+    AdresName text, 
+    AdressZIP text, 
+    PhoneNum text, 
+    Sex varchar(10), 
+    Password text, 
+    primary key(ID)
+    );
+create table wineshop.products(
+    ID int not null auto_increment,
+    Domain text,
+    Type varchar(50),
+    Origin text,
+    Price double,
+    Image text,
+    primary key(ID)
+);
+create table wineshop.cart (
+    ID int not null auto_increment,
+    UserID int, 
+    ProductID int, 
+    Quantity int, 
+    State varchar(10), 
+    primary key(ID),
+    foreign key(UserID) references client(ID),
+    foreign key(ProductID) references products(ID)
+    );
+create table wineshop.orders(
+    ID int not null auto_increment,
+    UserID int,
+    Total double,
+    CartIDs text,
+    primary key(ID),
+    foreign key(UserID) references client(ID)
+);
